b3d3e5d0bfec713477691b2514ccb0bdfbb3d85d
Tao Wang <moonavw@gmail.com>
2014-05-18 23:49:46 -0700

    - support git branch pulling

--- Infrastructure.Git/GitSourcePuller.cs
+++ Infrastructure.Git/GitSourcePuller.cs
@@ -3,24 +3,58 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using RunProcessAsTask;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace TestLab.Infrastructure.Git
 {
     public class GitSourcePuller : ISourcePuller
     {
+        private static readonly Regex RxUrl = new Regex(@"http(s)?://.+/\w+\.git", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         #region Implementation of ITestPuller
 
         public bool CanPull(string repoPathOrUrl)
         {
-            return repoPathOrUrl.EndsWith(".git", StringComparison.OrdinalIgnoreCase);
+            return RxUrl.IsMatch(repoPathOrUrl);
         }
 
         public Task Pull(string repoPathOrUrl, string workDir)
         {
+            var ss = repoPathOrUrl.Split(' ');
+            string branch=null, url=ss[0];
+            if (ss.Length > 1)
+            {
+                branch = ss[1];
+            }
+
+            ProcessStartInfo pi;
+
+            var args = new List<string>();
+            
             //git clone or git pull
-            var pi = !Directory.Exists(workDir)
-                ? new ProcessStartInfo("git", string.Format("clone {0} {1}", repoPathOrUrl, workDir))
-                : new ProcessStartInfo("git", string.Format("pull {0}", repoPathOrUrl)) { WorkingDirectory = workDir };
+            if (Directory.Exists(workDir))
+            {
+                args.Add("pull");
+                args.Add(url);
+                if (!string.IsNullOrEmpty(branch))
+                {
+                    args.Add(branch);
+                }
+                pi = new ProcessStartInfo("git", string.Join(" ", args)) { WorkingDirectory = workDir };
+            }
+            else
+            {
+                args.Add("clone");
+                args.Add(url);
+                if (!string.IsNullOrEmpty(branch))
+                {
+                    args.Add("-b");
+                    args.Add(branch);
+                }
+                args.Add(workDir);
+                pi = new ProcessStartInfo("git", string.Join(" ", args));
+            }
 
             return ProcessEx.RunAsync(pi);
         }
